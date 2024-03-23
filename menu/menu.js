f3a480056b1e22b67b929122890756be9201af65
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-03-23 16:33:19 +0100

    ETI5

--- menu/menu.js
+++ menu/menu.js
@@ -1,31 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let path = window.location.pathname;
-    let menuLinks = document.querySelectorAll('.menu .icon');
+// Получаем текущий путь страницы
+let currentPath = window.location.pathname;
 
-    menuLinks.forEach(function(link) {
-        let linkPath = link.getAttribute('href');
-        if (linkPath === path) {
-            link.classList.add('active');
-            localStorage.setItem('selectedMenuItem', linkPath); // Сохраняем выбранный пункт меню в localStorage
-        }
-    });
-});
-
-// Обрабатываем клик на пункте меню
-document.querySelectorAll('.menu .icon').forEach(function(link) {
-    link.addEventListener('click', function() {
-        localStorage.setItem('selectedMenuItem', link.getAttribute('href')); // Сохраняем выбранный пункт меню в localStorage
-    });
-});
-
-// При загрузке страницы проверяем, есть ли сохраненный выбранный пункт меню в localStorage
-document.addEventListener("DOMContentLoaded", function() {
-    let selectedMenuItem = localStorage.getItem('selectedMenuItem');
-    if (selectedMenuItem) {
-        document.querySelectorAll('.menu .icon').forEach(function(link) {
-            if (link.getAttribute('href') === selectedMenuItem) {
-                link.classList.add('active'); // Добавляем класс active к выбранному пункту меню
-            }
-        });
+// Находим ссылку в меню с текущим путем и добавляем класс "active"
+let menuLinks = document.querySelectorAll('.menu a');
+menuLinks.forEach(function(link) {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('active');
     }
 });
