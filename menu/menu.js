279c10905d245bbb15d7b5dfcc21733b59671b8a
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-03-23 16:23:28 +0100

    ETI4

--- menu/menu.js
+++ menu/menu.js
@@ -1,12 +1,31 @@
-let currentUrl = window.location.pathname;
+document.addEventListener("DOMContentLoaded", function() {
+    let path = window.location.pathname;
+    let menuLinks = document.querySelectorAll('.menu .icon');
 
-// Получаем все ссылки в меню
-let menuLinks = document.querySelectorAll('#menu a');
+    menuLinks.forEach(function(link) {
+        let linkPath = link.getAttribute('href');
+        if (linkPath === path) {
+            link.classList.add('active');
+            localStorage.setItem('selectedMenuItem', linkPath); // Сохраняем выбранный пункт меню в localStorage
+        }
+    });
+});
 
-// Проходимся по каждой ссылке
-menuLinks.forEach(function(link) {
-    // Если URL ссылки совпадает с текущим URL страницы, добавляем класс 'active'
-    if (link.getAttribute('href') === currentUrl) {
-        link.classList.add('active');
+// Обрабатываем клик на пункте меню
+document.querySelectorAll('.menu .icon').forEach(function(link) {
+    link.addEventListener('click', function() {
+        localStorage.setItem('selectedMenuItem', link.getAttribute('href')); // Сохраняем выбранный пункт меню в localStorage
+    });
+});
+
+// При загрузке страницы проверяем, есть ли сохраненный выбранный пункт меню в localStorage
+document.addEventListener("DOMContentLoaded", function() {
+    let selectedMenuItem = localStorage.getItem('selectedMenuItem');
+    if (selectedMenuItem) {
+        document.querySelectorAll('.menu .icon').forEach(function(link) {
+            if (link.getAttribute('href') === selectedMenuItem) {
+                link.classList.add('active'); // Добавляем класс active к выбранному пункту меню
+            }
+        });
     }
 });
